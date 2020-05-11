4cd9e27bd38c0c9c8e2ea1c61589107116d2acd4
huyanhtran <trananhhuy1991@gmail.com>
2020-05-11 23:00:05 +0700

    Add file JS

---
+++ main.js
@@ -0,0 +1,35 @@
+//define variables
+let amount = prompt("How much amount you want to convert?")
+let currencyRatio = 23161.39
+
+
+//IF-ELSE condition
+
+if(isNaN(amount)){
+  console.log("Input a valid number please!!")
+}else{
+  let from = prompt("Which currency do you have?")
+  let to = prompt("Which currency do you want?")
+  if(from === "usd" && to === "vnd"){
+    console.log(from," to ", to, "is ", formatCurrency(VND, usdToVnd()))
+  }else if (from === "vnd" && to === "usd"){
+    console.log(from," to ",to, "is ", formatCurrency(USD, vndToUsd()))
+  }else{
+    console.log("Wrong currency. Please input again !!!")
+  }
+}
+
+//Function
+function usdToVnd(){
+  return result = amount * currencyRatio
+}
+function vndToUsd(){
+  return result = amount / currencyRatio    
+}
+function formatCurrency(type, value) {
+    const formatter = new Intl.NumberFormat(type, {
+      currency: type,
+      style: "currency"
+    });
+    return formatter.format(value);
+}
